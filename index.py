958c35048abd4c37f21c2f25ddec2dfabb538159
Tyler Long <tyler4long@gmail.com>
2017-05-18 13:17:09 +0800

    Initial commit

---
+++ index.py
@@ -0,0 +1,27 @@
+from oauth2client.service_account import ServiceAccountCredentials
+
+scopes = ['https://www.googleapis.com/auth/androidpublisher']
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name('./keyfile.json', scopes=scopes)
+
+from httplib2 import Http
+
+http = Http()
+
+http_auth = credentials.authorize(http)
+
+resp, content = http.request('https://www.googleapis.com/androidpublisher/v2/applications/com.glip.mobile/reviews', method='GET')
+print resp
+print content
+
+print '================'
+
+resp, content = http.request('https://www.googleapis.com/androidpublisher/v2/applications/com.ringcentral.android/reviews', method='GET')
+print resp
+print content
+
+print '================'
+
+resp, content = http.request('https://www.googleapis.com/androidpublisher/v2/applications/com.ringcentral.meetings/reviews', method='GET')
+print resp
+print content
